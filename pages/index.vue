df4837806a3a90696c6711b0b3a0ccef5767bbef
muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2022-04-08 19:23:49 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		Hello nuxtjs
+		<p>Lorem ipsum dolor sit amet, consectetur</p>
+		<p>Lorem ipsum dolor sit amet</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'IndexPage',
+}
+</script>
